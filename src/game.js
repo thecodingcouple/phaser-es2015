578402f5ffddb3ee12be2c96dae22adc53af33c4
townsean <ashley.grenon@gmail.com>
2016-07-22 23:00:22 -0500

    moved  construction logic to default initial state

--- src/game.js
+++ src/game.js
@@ -9,14 +9,12 @@ import { Victory } from './states/victory';
 
 export class Game extends Phaser.Game {
     constructor() {
-        super(800, 600, Phaser.AUTO, 'phaser-canvas', null);
-        
-        this.state.add('load', Load);
-        this.state.add('menu', Menu);
-        this.state.add('play', Play);
-        this.state.add('gameover', GameOver);
-        this.state.add('victory', Victory);
-        
-        this.state.start('load');
-    }
+        super(800, 600, Phaser.AUTO, 'phaser-canvas', { create : () => {            
+            this.state.add('load', Load);
+            this.state.add('menu', Menu);
+            this.state.add('play', Play);
+            this.state.add('gameover', GameOver);
+            this.state.start('load');
+        }});        
+    }  
 }
